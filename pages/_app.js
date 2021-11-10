2946f2e606d4a51141766f9ca126fb9d3760311b
bored-wizard <sujanbarman12345q@gmail.com>
2021-11-10 13:45:03 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Web3ReactProvider } from '@web3-react/core'
+import 'tailwindcss/tailwind.css'
+import Web3 from 'web3'
 import '../styles/globals.css'
 
+const getLibrary = (provider) => {
+  return new Web3(provider)
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
